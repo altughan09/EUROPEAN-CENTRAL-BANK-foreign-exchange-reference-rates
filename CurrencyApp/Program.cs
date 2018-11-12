ab100845d30e0cd7187dca4706b9f7a8fa7a6da1
Altughan Ozengi <31768522+altugozengi@users.noreply.github.com>
2018-11-12 19:27:59 +0100

    Add files via upload

---
+++ CurrencyApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace CurrencyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+            xmlDoc.Load("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
+
+            Console.WriteLine("EUROPEAN CENTRAL BANK - Euro foreign exchange reference rates");
+            foreach (XmlNode xmlNode in xmlDoc.DocumentElement.ChildNodes[2].ChildNodes[0].ChildNodes)
+                Console.WriteLine(xmlNode.Attributes["currency"].Value + ": " + xmlNode.Attributes["rate"].Value);
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
